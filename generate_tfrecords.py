c5d913ec7b9e9be7c0419be894f893bfa5d9bf92
gvt217 <gvt217@nyu.edu>
2020-12-14 16:53:26 -0500

    Rename data_utilv2.py to generate_tfrecords

--- generate_tfrecords.py
+++ generate_tfrecords.py
@@ -1,12 +1,11 @@
-import tensorflow_datasets as tfds
-import tensorflow as tf
 import json
+import os
+import random
+import shutil
 from glob import glob
 from pathlib import Path
-from scipy.io import wavfile
-import random
-import os
-import shutil
+
+import tensorflow as tf
 
 sample_rate = 16000
 
@@ -20,12 +19,12 @@ def _bytes_feature(value):
 
 def _float_feature(value):
     """Returns a float_list from a float / double."""
-    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
 
 
 def _int64_feature(value):
     """Returns an int64_list from a bool / enum / int / uint."""
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
 
 def train_test_val_split(path_to_wavs):
@@ -47,15 +46,15 @@ def train_test_val_split(path_to_wavs):
     return train_paths, test_paths, val_paths
 
 
-def create_ds_folder(train, test, val):
+def create_ds_folder(base_dir):
     try:
-        shutil.rmtree('reverbsynth_raw')
+        shutil.rmtree(base_dir + 'reverbsynth_raw')
     except:
         pass
     try:
-        os.makedirs('reverbsynth_raw/train')
-        os.makedirs('reverbsynth_raw/test')
-        os.makedirs('reverbsynth_raw/valid')
+        os.makedirs(base_dir + 'reverbsynth_raw/train')
+        os.makedirs(base_dir + 'reverbsynth_raw/test')
+        os.makedirs(base_dir + 'reverbsynth_raw/valid')
     except:
         pass
 
@@ -78,35 +77,41 @@ def write_records(paths, metadata, output_path):
         # Write records
         for index, path in enumerate(paths):
             # read audio file
-            sample_rate, audio_array = wavfile.read(path)
-
-            # select only the first 64000 entries
-            audio_array = audio_array[:64000]
+            raw_audio = tf.io.read_file(path)
+            print(path)
+            audio_array, sample_rate = tf.audio.decode_wav(raw_audio, desired_channels=1, desired_samples=64000)
 
             key = Path(path).stem
             data = metadata[key]
             pitch = data['pitch']
 
-            example = to_tfrecord(audio_array, pitch)
+            example = to_tfrecord(audio_array.numpy().flatten().tolist(), pitch)
             writer.write(example.SerializeToString())
 
 
-def generate_tfrecords(path_to_wavs, path_to_meta):
+def generate_tfrecords(path_to_wavs, path_to_meta, output_dir):
     train_paths, test_paths, val_paths = train_test_val_split(path_to_wavs)
     print(f"train_records: {len(train_paths)}\ntest_records: {len(test_paths)}\nval_records: {len(val_paths)}")
+
+    if output_dir[-1] not in ['/', '\\']:
+        output_dir += '/'
     # create raw dataset
-    create_ds_folder(train_paths, test_paths, val_paths)
+    create_ds_folder(output_dir)
 
     # Load metadata
     with open(path_to_meta) as f:
         metadata = json.load(f)
 
     print("Generating train records")
-    write_records(train_paths, metadata, 'reverbsynth_raw/train/reverbsynth-train.tfrecord')
+    write_records(train_paths, metadata, output_dir + 'reverbsynth_raw/train/reverbsynth-train.tfrecord')
     print("Generating test records")
-    write_records(test_paths, metadata, 'reverbsynth_raw/test/reverbsynth-test.tfrecord')
+    write_records(test_paths, metadata, output_dir + 'reverbsynth_raw/test/reverbsynth-test.tfrecord')
     print("Generating val records")
-    write_records(val_paths, metadata, 'reverbsynth_raw/valid/reverbsynth-valid.tfrecord')
+    write_records(val_paths, metadata, output_dir + 'reverbsynth_raw/valid/reverbsynth-valid.tfrecord')
 
 
-generate_tfrecords(r'C:\Users\ghana\Documents\DL\new_data\audio', r'C:\Users\ghana\Documents\DL\new_data\examples.json')
+if __name__ == '__main__':
+    wavs = input('Enter the path to the audio wav files')
+    meta = input('Enter the path to the json meta file')
+    op_dir = input('Enter the output directory')
+    generate_tfrecords(wavs, meta, op_dir)
